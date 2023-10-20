2cbee17fd264f3c6f827db07dce850f8cf75fe92
구제서 <jsgu@koreanair.com>
2023-10-20 17:37:34 +0900

    commit

---
+++ main/User/KE/CDSO/ssotester copy 2.tf
@@ -0,0 +1,8 @@
+module "create-user-ssotester2" {
+  source = "../../../../modules/User"
+  display_name = "ssotester2@koreanair.com"
+  user_name    = "ssotester2"
+  given_name  = "ssotester2"
+  family_name = "ssotester2"
+  emails = "ssotester2@koreanair.com"
+}
---
+++ main/User/KE/CDSO/ssotester copy.tf
@@ -0,0 +1,8 @@
+module "create-user-ssotester3" {
+  source = "../../../../modules/User"
+  display_name = "ssotester3@koreanair.com"
+  user_name    = "ssotester3"
+  given_name  = "ssotester3"
+  family_name = "ssotester3"
+  emails = "ssotester3@koreanair.com"
+}
