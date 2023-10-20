57129e4a0275605f5f4053b558d88fcd780fab73
구제서 <jsgu@koreanair.com>
2023-10-20 17:24:59 +0900

    commit

--- main/User/KE/CDSO/kyungtaekhong.tf
+++ main/User/KE/CDSO/kyungtaekhong.tf
@@ -6,9 +6,3 @@ module "create-user-kyungtaekhong" {
   family_name = "Hong"
   emails = "kyungtaekhong@koreanair.com"
 }
-
-module "attach-group-kyungtaekhong" {
-  source = "../../../../modules/Membership"
-  group_name = "GRP-CDSO-ADMIN"
-  user_id = module.create-user-kyungtaekhong.user_id
-}
